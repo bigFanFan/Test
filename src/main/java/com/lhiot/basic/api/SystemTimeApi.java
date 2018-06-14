52be3367e8a2ce5f835451074f06a84c4265eabc
limiaojun <limiaojun9116@163.com>
2018-06-14 16:09:10 +0800

    增加restcontroller

--- src/main/java/com/lhiot/basic/api/SystemTimeApi.java
+++ src/main/java/com/lhiot/basic/api/SystemTimeApi.java
@@ -1,15 +1,19 @@
 package com.lhiot.basic.api;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description: 系统时间接口
  * @author: Limiaojun
  * @create: 2018-06-14 16:01
  **/
+@Api(description = "系统时间接口")
+@RestController
 @RequestMapping("/system/time")
 public class SystemTimeApi {
 
